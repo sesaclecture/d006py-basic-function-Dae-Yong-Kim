54cd2799b8cb36bcfe2cf0132c2e2d2da33eebf8
Daeyong Kim <youbinlove607@gmail.com>
2025-09-02 11:15:00 +0900

    Submit Basic Function Assignment

--- basic_func/basic_func.py
+++ basic_func/basic_func.py
@@ -1,26 +1,35 @@
 def add(a, b):
-    pass
+    return a + b
 
 
 def sub(a, b):
-    pass
+    return a - b
 
 
 def mul(a, b):
-    pass
+    return a * b
 
 
 def div(a, b):
-    pass
+    return a / b
 
 
 def power(base, pow):
-    pass
+    return base**pow
 
 
 def square(base):
-    pass
+    return base**2
 
 
 def greet(이름="낯선자", 나이=20):
-    pass
+    ret_str = ""
+    if 나이 >= 50:
+        ret_str += "안녕하십니까 "
+    elif 나이 >= 20:
+        ret_str += "안녕하신가 "
+    else:
+        ret_str += "안녕 "
+    ret_str += f"{이름}!"
+    return ret_str
+
